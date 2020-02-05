7d664e4471d8d8c52b4be670dddd125c549886b7
MrOffer <thomas.e.offer@gmail.com>
2020-02-05 14:29:19 -0600

    fixed home page

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -1,4 +1,5 @@
 package com.codeup.springblog.controllers;
+import com.codeup.springblog.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +8,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
+    private PostRepository postDao;
+
+    public HomeController(PostRepository postDao){
+        this.postDao = postDao;
+    }
 
     @GetMapping("/home/{user}")
 
     public String homePage(@PathVariable String user, Model model){
         model.addAttribute("user",user);
+        model.addAttribute("posts", postDao.findAll());
         return "home";
     }
 }
